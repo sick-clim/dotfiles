6591da515f1c389b7ae411b1b49b8a3e573cdef8
sick-clim <46617682+sick-clim@users.noreply.github.com>
2024-03-15 19:56:42 +0900

    add .config directory

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -5,21 +5,35 @@ readonly DOTFILES=(
   .vimrc
   .zshrc
   .zshenv
-  .tool-versions
+#  .tool-versions
   .config/nvim/init.vim
   .config/tmux/statusline.conf
   .config/tmux/tmux.conf
-  .config/lvim/config.lua
+#  .config/lvim/config.lua
 )
 
-which ghq
+# aqua 使用案
+# install aqua
+# temp にaqua 設定ファイルGET
+# ghq でget
+# aqua install
+
+# mise 使用案
+# install mise with berw
+# install ghq from mise
+# setting ghq
+
+type brew
 
 ghq get sick-clim/dotfiles
 repo_path=$(ghq list dotfiles)
 ghq_root=$(ghq root)
 cd $ghq_root/$repo_path
 
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config
+for d in nvim tmux; do
+    mkdir -p ~/.config/${d}
+done
 
 for f in ${DOTFILES[@]}; do
     ln -snfv ${PWD}/${f} ~/${f}
